7bfc7e84e9d259d4ac1d7d2aa3a21649101f7bc1
Trevor Nweze <106932464+trevornweze@users.noreply.github.com>
2025-12-19 21:03:35 +0000

    Create VideoPlay component to display video title

---
+++ VideoPlay.jsx
@@ -0,0 +1,12 @@
+export default function VideoPlay(){
+   return(
+     <>
+       <h1>
+         {Video.Title}
+       </h1>
+       <button>
+         play
+         <img src=""></img>
+       </button>
+     </>
+   )};
